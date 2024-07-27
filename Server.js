4b470c2d521ccc5042926be3443b3d2f67b28182
Lanseey <ldcabe@student.hau.edu.ph>
2024-07-27 23:36:43 +0800

    Server.js

--- Server.js
+++ Server.js
@@ -24,6 +24,6 @@ var server = http.createServer(function (req, res) {
   }
 });
 
-server.listen(22000); // - listen for any incoming requests..
+server.listen(5000); // - listen for any incoming requests..
 
 console.log('Node.js web server at port 5000 is running.. :D');
